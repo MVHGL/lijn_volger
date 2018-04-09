89fba3b3ce3fef1a81da4d7e0cf907ff5c69de01
Ramon <ramon.vanbemmel@student.hu.nl>
2018-04-09 13:03:29 +0200

    V0.4

--- ontwijken.cpp
+++ ontwijken.cpp
@@ -12,30 +12,22 @@ using namespace std;
 
 BrickPi3 BP;	// instance of BrickPi3 class
 
-int debug = 1;
-unsigned int switch_afstand=5;
-int onoff=0;
+//DEBUG  
+int debug = 0;//zet op 1 voor debug. 
+int debug_ultrasoon=0; 
+
+//variabelen.
+
+unsigned int switch_afstand=10;
 unsigned int afstand=100;
 
-//function read_distance init
+//Gebruikte functies. 
 unsigned int read_distance();
-
-// Signal handler that will be called when Ctrl+C is pressed to stop the program
-void exit_signal_handler(int signo)
-{
-	if(signo == SIGINT)
-	{
-		BP.reset_all();    // Reset everything so there are no run-away motors
-		exit(-2);
-	}
-}
+void dodge(); 
+void exit_signal_handler(int signo);
+void setup();
 
 
-void setup()
-{
-	signal(SIGINT, exit_signal_handler); // Ctrl-c signal handler
-	BP.detect(); // Check for BrickPi firmware
-}
 int main()
 {
 	setup();
@@ -53,59 +45,19 @@ int main()
 	// Main loop
 	while (true)
 	{
-
-	
-
 		if (BP.get_sensor(PORT_3, right_light) == 0 && BP.get_sensor(PORT_4, left_light) == 0)
 		{
 			afstand=read_distance(); 
-		//	sleep(0.8);
 			if (afstand <= switch_afstand && afstand) // als er een object is gaat die hem hier ontwijken. 
 			{	
-				cout << read_distance() << endl;
-				cout <<"in object gevonden if" <<endl;
-				BP.set_motor_limits(PORT_B, 40, 2000);
-				BP.set_motor_limits(PORT_C, 40, 2000);
-				// maakt bocht van 90 graden naar rechts
-				BP.set_motor_power(PORT_C, 0);
-				BP.set_motor_position_relative(PORT_B, 360);
-				sleep(1);	
-				//m klein stukje naar voren ( 1 slag = 1100)
-				BP.set_motor_position_relative(PORT_B, 180);
-				BP.set_motor_position_relative(PORT_C, 180);
-				sleep(1);
-				// maakt bocht naar links 90 graden. 
-				BP.set_motor_power(PORT_B, 0);
-				BP.set_motor_position_relative(PORT_C, 360);
-				sleep(1);
-				//bepaalde afstand naar voren
-				BP.set_motor_position_relative(PORT_B, 50);
-				BP.set_motor_position_relative(PORT_C, 50);
-				sleep(1);
-				//bocht naar links 90 graden
-				BP.set_motor_power(PORT_B, 0);
-				BP.set_motor_position_relative(PORT_C, 360);
-				sleep(1);
-				//rechtdoor
-				BP.set_motor_position_relative(PORT_B, 360);
-				BP.set_motor_position_relative(PORT_C, 360);
-				sleep(1);
-				// bochtje naar rechts 
-				BP.set_motor_power(PORT_C, 0);
-				BP.set_motor_position_relative(PORT_B, 100);		
-				sleep(1);
+				dodge();
 			}
-			//cout << "in init if" << endl;	// read value from sensors
-			cout << read_distance() << endl;				
+			
+			if(debug_ultrasoon)
+				cout << read_distance() << endl;				
 			cout<<"Volgt de lijn" <<endl;		
-			//cout << "Rechts:\n";
 			prop_power(R_VAL, false); // steer proportionally on the right
-			//cout << "Links:\n";
 			prop_power(L_VAL, true);  // steer proportionally on the left
-	//		sleep(1);
-			//onoff=1
-				
-
 		}
 	}
 }
@@ -119,30 +71,69 @@ unsigned int read_distance() 		//Dit is de functie voor het lezen van de afstand
 		a=Ultrasonic2.cm;
 
 		b=Ultrasonic2.cm;
-		
-		cout<<"ULTRASOON functie \t" <<Ultrasonic2.cm <<endl; 
+		if(debug_ultrasoon)
+			cout<<"ULTRASOON functie \t" <<Ultrasonic2.cm <<endl; 
 		while (a ==0)
 		{
 			a=Ultrasonic2.cm;
-
-			/*t++;
-			if (t==7)
-			{
-				break; 
-			}*/
 		}
-		//t=0;
 		while (b==0)
 		{
 			b=Ultrasonic2.cm;
-
-			//t++;
-			/*if (t==7)
-			{
-				break;
-			}*/
 		}
 		return (a+b)/2;
 	}
     
   }
+void setup()
+{
+	signal(SIGINT, exit_signal_handler); // Ctrl-c signal handler
+	BP.detect(); // Check for BrickPi firmware
+}
+void dodge()
+{
+	float delay=1;
+	//cout << read_distance() << endl;
+	cout <<"in object gevonden if" <<endl;
+	BP.set_motor_limits(PORT_B, 40, 2000);
+	BP.set_motor_limits(PORT_C, 40, 2000);
+	// maakt bocht van 90 graden naar rechts
+	BP.set_motor_power(PORT_C, 0);
+	BP.set_motor_position_relative(PORT_B, 360);
+	sleep(1);	
+	//m klein stukje naar voren ( 1 slag = 1100)
+	BP.set_motor_position_relative(PORT_B, 180);
+	BP.set_motor_position_relative(PORT_C, 180);
+	sleep(delay);
+	// maakt bocht naar links 90 graden. 
+	BP.set_motor_power(PORT_B, 0);
+	BP.set_motor_position_relative(PORT_C, 360);
+	sleep(delay);
+	//bepaalde afstand naar voren
+	BP.set_motor_position_relative(PORT_B, 500);
+	BP.set_motor_position_relative(PORT_C, 500);
+	sleep(delay);
+	//bocht naar links 90 graden
+	BP.set_motor_power(PORT_B, 0);
+	BP.set_motor_position_relative(PORT_C, 360);
+	sleep(delay);
+	//rechtdoor
+	BP.set_motor_position_relative(PORT_B, 70);
+	BP.set_motor_position_relative(PORT_C, 70);
+	sleep(delay);
+	// bochtje naar rechts 
+	BP.set_motor_power(PORT_C, 0);
+	BP.set_motor_position_relative(PORT_B, 100);		
+	sleep(delay);
+	//while (sensor1 != zwart || sensor2 != zwart) 
+		//rechtdoor
+}
+// Signal handler that will be called when Ctrl+C is pressed to stop the program
+void exit_signal_handler(int signo)
+{
+	if(signo == SIGINT)
+	{
+		BP.reset_all();    // Reset everything so there are no run-away motors
+		exit(-2);
+	}
+}
