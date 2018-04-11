c8ef76f9a3687768c7118bb4591c9985e7608520
ramonvbemmel <31732080+ramonvbemmel@users.noreply.github.com>
2018-04-11 13:06:51 +0200

    Commentaar toegevoegd

--- src/course_one/motor_control.cpp
+++ src/course_one/motor_control.cpp
@@ -4,16 +4,16 @@
 #include <unistd.h>
 
 BrickPi3 MyBP; // new instance of BrickPi3
-int debug_motor=0;
+int debug_motor=0;	//Zet op 1 om te debuggen
 
-void run_motor(int left_power, int right_power)
+void run_motor(int left_power, int right_power)		//Zet de motorpower op ingegeven waarde. 
 {
 	MyBP.set_motor_power(PORT_C, right_power);
 	MyBP.set_motor_power(PORT_B, left_power);
 }
 
 
-void prop_power(int sensor_value, bool left_sensor)
+void prop_power(int sensor_value, bool left_sensor, &debug)	//Functie voor propotioneel bij sturen.
 {
 	int Kp = 2;	// Proportional constant
 	int right_offset = 24; // offset for right sensor
@@ -27,8 +27,9 @@ void prop_power(int sensor_value, bool left_sensor)
 	if (left_sensor){
 		sensor_value = sensor_value / 6;
 		int error = sensor_value - left_offset;
-		printf("ERROR: %d\n", error);
-		printf("SENSOR_V: %d\n", sensor_value);
+		if(debug){
+			printf("ERROR: %d\n", error);
+			printf("SENSOR_V: %d\n", sensor_value);}
 		int turn = Kp * error;
 
 		motor_left = Tp - turn;
@@ -37,16 +38,17 @@ void prop_power(int sensor_value, bool left_sensor)
 	else{
 		sensor_value = sensor_value / 80;
 		int error = sensor_value - right_offset;
-		printf("ERROR: %d\n", error);
-		printf("SENSOR_V: %d\n", sensor_value);
+		if(debug){
+			printf("ERROR: %d\n", error);
+			printf("SENSOR_V: %d\n", sensor_value);}
 		int turn = Kp * error;
 
 		motor_left = Tp + turn;
 		motor_right = Tp - turn;
 
 	}
-
-	printf("Motor left: %d\n", motor_left);
-	printf("Motor right: %d\n", motor_right);
+	if(debug){
+		printf("Motor left: %d\n", motor_left);
+		printf("Motor right: %d\n", motor_right);}
 	run_motor(motor_left, motor_right);
 }
